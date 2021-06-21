664771a1ede1f6d2f09a4e4cf58e8a8cc7f1411d
AKHIL-S-BABU <53579216+AKHIL-S-BABU@users.noreply.github.com>
2021-06-21 18:44:23 +0530

    Update P02_short-form.c

--- Programs/P02_short-form.c
+++ Programs/P02_short-form.c
@@ -1,3 +1,31 @@
+Implement the following scenario using C programming language.
+The user has to read various abbreviations from the keyboard and construct
+acronyms for the phrases entered (you may avoid the prepositions in the
+phrases read). The Acronyms must be displayed in the sorted order and must
+be displayed in capital letters.
+*
+*
+*
+*
+*
+*Test case
+Input
+World Health Organization
+American standard code for information interchange
+Do it yourself
+As soon as Possible
+Laugh Out Loud
+
+Output
+ASCII
+ASAP
+DIY
+LOL
+WHO
+******
+*******
+********
+
 #include <stdio.h>
 #include<string.h>
 #include<conio.h>
@@ -9,11 +37,16 @@ int main()
     char c[15][50];
     printf("Enter the number of strings:");
     scanf("%d",&n);
+    //to enter the string from input into a 2D array
+   
     for(i=0;i<n;i++)
     {
         printf("Enter string %d:",i+1);
         scanf(" %[^\n]%*c",&strn[i]);
     }
+    
+    //split the words in each sentence and then taking only the first alpabet of each word
+    
     for(i=0;i<n;i++)
     {
         int k=1;
@@ -29,10 +62,16 @@ int main()
         }
         c[i][k]='\0';
     }
+    
+    //to make the alphabet in UPPERCASE i used strupr
+    
     for(i=0;i<n;i++)
     {
         strupr(c[i]);
     }
+    
+    //to sort the array of alphabet in alphabetic order
+    
     for(i=0;i<n-1;i++)
     {
         for(j=0;j<n-i-1;j++)
@@ -45,6 +84,8 @@ int main()
             }
         }
     }
+    
+    // at last print the alphabets in the array
     printf("The Short forms are: ");
     for(i=0;i<n;i++)
     {
