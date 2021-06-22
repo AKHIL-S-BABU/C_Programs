903c55d11b06d18d5ff456779fbed50bdf97e3b1
AKHIL-S-BABU <53579216+AKHIL-S-BABU@users.noreply.github.com>
2021-06-22 18:28:55 +0530

    Update P03_Selection_sort.c

--- Programs/P03_Selection_sort.c
+++ Programs/P03_Selection_sort.c
@@ -1,3 +1,11 @@
+//Author: AKHIL S BABU
+
+//Implementation of selection sort with an example
+//Selection sort iterates all the elements and if the smallest element in the list is found then that number of selection sort 
+// Then it is swapped with the first element of the array
+//A seperation occurs with sorted an unsorted elements
+
+
 #include<stdio.h>
 
 int sorted(int arr[],int b,int n)
@@ -19,6 +27,7 @@ int sorted(int arr[],int b,int n)
         
     }
 }
+// main program to check the above function
 int main()
 {
     int arr[]={7,18,55,23,86,99,8,61};
@@ -32,3 +41,12 @@ int main()
     return 0;
 
 }
+
+
+Output:
+The sorted array is : 
+7       8       18      23      55      61      86      99
+
+    
+    
+    
